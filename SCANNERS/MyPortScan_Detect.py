742cace673537298af5f2dba46203f203479280d
leitn <leila.tnitan.pro@gmail.com>
2024-10-16 18:38:21 +0200

    OK

--- SCANNERS/MyPortScan_Detect.py
+++ SCANNERS/MyPortScan_Detect.py
@@ -17,6 +17,8 @@ PORTS=[]
 def portscan_detect(pkt):
     # Get the source IP address
     src_ip = pkt[IP].src
+    port_count = {}
+    scanners = {}
     cur_time = time.time()
  
     # Get the source port number
@@ -25,29 +27,26 @@ def portscan_detect(pkt):
     dis_port = tcp_header.dport
  
         # Check if this is a new connection
-        if (src_ip, src_port) not in self.last_packet_time:
-            self.last_packet_time[(src_ip, src_port)] = time.time()
-            self.port_count[(src_ip, src_port)] = 1
-            return
- 
-        # Check if this is a connection to a new port
-        last_packet_time = self.last_packet_time[(src_ip, src_port)]
-        if time.time() - last_packet_time > WINDOW:
-            self.port_count[(src_ip, src_port)] = 0
-        self.last_packet_time[(src_ip, src_port)] = time.time()
-        self.port_count[(src_ip, src_port)] += 1
-        counts = self.port_count[(src_ip, src_port)] 
+    if (src_ip, src_port) not in last_packet_time:
+        last_packet_time[(src_ip, src_port)] = time.time()
+        port_count[(src_ip, src_port)] = 1
+     
+    # Check if this is a connection to a new port
+    last_packet_time = last_packet_time[(src_ip, src_port)]
+    if time.time() - last_packet_time > WINDOW:
+        port_count[(src_ip, src_port)] = 0
+        last_packet_time[(src_ip, src_port)] = time.time()
+        port_count[(src_ip, src_port)] += 1
+        counts = port_count[(src_ip, src_port)] 
         PORTS.append(dis_port)
         # Check if we have reached the threshold for this connection
-        dates=self.scanners.add(src_ip) 
-        if counts >= THRESHOLD:
-            self.scanners.add(src_ip)
-            print(f"Scanner detected. The scanner originated from host {src_ip}.")
-            AllPorts = '\n'.join(map(str,PORTS))
-            print("Ports Scanned [",AllPorts,"]")
-            quit()
-
-
+    dates=scanners(src_ip) 
+    if counts >= THRESHOLD:
+        scanners.append(src_ip)
+        print(f"Scanner detected. The scanner originated from host {src_ip}.")
+        AllPorts = '\n'.join(map(str,PORTS))
+        print("Ports Scanned [",AllPorts,"]")
+        quit()
  
 def main():
     sniff(prn=portscan_detect, filter="tcp", store=0)
--- SCANNERS/my_logging.py
+++ SCANNERS/my_logging.py
@@ -6,11 +6,11 @@ __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 def fill_log(str, pkt):
-	f = os.path.join(__location__, 'logs/log.txt')
+	f = os.path.join(__location__, 'log.txt')
 	if IP in pkt:
 		pkt_addr = pkt[IP].src
 	with open(f, 'a') as file:
 		n = time.time()
-		time_format = time.strftime("%H:%M:%S", time.gmtime(n))
-		file.write(f"[time: {time_format}] [IP : {pkt_addr} ]: {str}")
+		time_format = time.strftime("%D:%H:%M:%S", time.gmtime(n))
+		file.write(f"[{time_format}] [IP : {pkt_addr} ]: {str}")
 	return 0
